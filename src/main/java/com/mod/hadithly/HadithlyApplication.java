fa2bd3f6555ee659fae9e6c6faa7291bee82f863
iljaasdhonre <iljaasd@outlook.com>
2023-01-17 19:04:33 +0100

    Initial commit

---
+++ src/main/java/com/mod/hadithly/HadithlyApplication.java
@@ -0,0 +1,13 @@
+package com.mod.hadithly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadithlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadithlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mod/hadithly/HadithlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mod.hadithly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadithlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
